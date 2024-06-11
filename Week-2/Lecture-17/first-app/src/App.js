1ec538045ee8d35050a96fae88a0af61b99c12bd
Bruhadev <devbruha@gmail.com>
2024-06-11 18:57:22 +0530

    Upload Lecture-17

---
+++ Week-2/Lecture-17/first-app/src/App.js
@@ -0,0 +1,19 @@
+
+import { Component } from "react";
+class App extends Component{
+  name="Bruhadev"
+  render(){
+    return(
+      <>
+      <div>
+        <h1>This is {this.name}</h1>
+      </div>
+      <div>
+        <p> This is paragraph my name is {this.name}</p>
+      </div>
+      </>
+    )
+  }
+}
+
+export default App;
