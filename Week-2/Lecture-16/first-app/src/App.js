348064109286401c70c66adedbf508152b910db6
Bruhadev <devbruha@gmail.com>
2024-06-11 11:51:42 +0530

    upload Lecture-16

---
+++ Week-2/Lecture-16/first-app/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+
+  const Name = 'Bruhadev'
+  return (
+    <>
+    <div>
+    <div>
+      <h1>
+        Hello from {Name}
+      </h1>
+      <h1>
+        Hello from Bruuu
+      </h1>
+
+      <h1 
+      style ={{
+        color:"green",
+        textAlign:"center",
+      }} >  Hello i am from Andhra </h1>
+    </div>
+    <p>This is dev</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
