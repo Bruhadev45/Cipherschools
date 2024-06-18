0cc157488b8065daeb98480ce0bf7fadab4412a1
Bruhadev <devbruha@gmail.com>
2024-06-18 16:12:26 +0530

    upload Lecture-22

---
+++ Week-3/Lecture-22/to-do/src/Components/AddTask.js
@@ -0,0 +1,16 @@
+const AddTask = () => {
+    return (
+        <div className="ui form">
+        <form>
+        <div className="field">
+    <label> Title</label>
+    <input type="text"/></div>
+    <div className="field"><label>Description</label> <textarea rows="2"></textarea>
+  </div>
+  <button className="ui primary button">5mmyh\Submit</button>
+        </form>
+        </div>
+    )
+};
+
+export default AddTask;
