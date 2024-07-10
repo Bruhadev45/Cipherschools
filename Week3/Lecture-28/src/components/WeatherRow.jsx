20d8919572314c0353683c18a326b67da555eca5
Bruhadev <devbruha@gmail.com>
2024-07-10 12:58:57 +0530

    update Lecture-28

--- Week3/Lecture-28/src/components/WeatherRow.jsx
+++ Week3/Lecture-28/src/components/WeatherRow.jsx
@@ -4,13 +4,13 @@ function WeatherRow() {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700  ">
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        Apple MacBook Pro 17"
+        17-07-2024
       </th>
       <td className="px-6 py-4">
-        Silver
+        34
       </td>
       <td className="px-6 py-4">
-        Laptop
+        Cloudy
       </td>
     </tr>
 
